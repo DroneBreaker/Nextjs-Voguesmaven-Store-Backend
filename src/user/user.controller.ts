9812e6b9d0ed6bdce5f97b5e579c3a6939e27ad2
DroneBreaker <brownierz01@gmail.com>
2022-11-27 01:38:19 +0000

    Initializing backend and added few changes

---
+++ src/user/user.controller.ts
@@ -0,0 +1,35 @@
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Req } from "@nestjs/common";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
+import { UserService } from "./user.service";
+
+@Controller()
+export class UserController{
+    constructor(private readonly userService: UserService) {}
+
+    @Get('/users')
+    allUsers() {
+        return this.userService.getUsers()
+    }
+
+    @Post('/users')
+    createUser(@Body() createUsetDto: CreateUserDto) {
+        return this.userService.create(createUsetDto)
+    }
+
+    @Patch('/users/:userId') 
+    updateUser(@Body() updateUserDto: UpdateUserDto, @Param( 'userId', ParseIntPipe) userId: number ) {
+        return this.userService.update(updateUserDto, userId)
+    }
+
+    @Get('/users/:userId') 
+    getUser(@Param('userId', ParseIntPipe) userId: number ) {
+        return this.userService.getUser(userId)
+    }
+
+    @Delete('/users/:userId') 
+    deleteUser(@Param('userId', ParseIntPipe) userId: number ) {
+        return this.userService.delete(userId)
+    }
+}
+
---
+++ src/user/user.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
+import { User } from "./entity/user.entity";
+
+@Injectable()
+export class UserService{
+    constructor(@InjectRepository(User) private userRepository: Repository<User>) {}
+
+    getUsers(): Promise<User[]> {
+        return this.userRepository.find()
+    }
+
+    create(createUserDto: CreateUserDto) {
+        return createUserDto
+    }
+
+    update(updateUserDto: UpdateUserDto, userId: number) {
+        return {body: updateUserDto, userId}
+    }
+
+    getUser(userId: number ) {
+        return { userId }
+    }
+
+    delete(userId: number) {
+        return { userId }
+    }
+}
